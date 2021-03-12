9c888a3a306c507d60336e8fd646879b6bbb6798
hopegipson <gipson.hope@gmail.com>
2021-03-12 16:24:26 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            const arr = state.friends.filter(function(friend) {
+                return friend.id !== action.id
+            })
+           return {friends: arr}
+        default:
+            return state
+    }
 }

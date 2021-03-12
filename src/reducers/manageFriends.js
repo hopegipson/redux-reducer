cbba155e83323323b0078dcd3368db9badd8445b
hopegipson <gipson.hope@gmail.com>
2021-03-12 16:24:09 -0600

    commit tree 35517d0f93c180dd020e21132a94e32743ecf27f

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            //const found = state.friends.findIndex(friend => friend.id === action.id) 
+            const arr = state.friends.filter(function(friend) {
+                return friend.id !== action.id
+            })
+           // const end = found + 1
+           // let friendsRemove = [state.friends.slice(found, 1)]
+           // console.log(friendsRemove)
+           return {friends: arr}
+
+           // return {friends: [state.friends.slice!(found, found+1)]}
+        default:
+            return state
+    }
 }
